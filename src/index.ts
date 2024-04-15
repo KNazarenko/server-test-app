4815870d675f18169b0abb93a08f4666f205d44c
nko <konstantyn.nazarenko@gmail.com>
2024-04-15 16:21:05 +0300

    test commit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-	let message = "Hello NKO!";
+	let message = "Hello NKO!!!";
 	res.send(message);
 });
 
